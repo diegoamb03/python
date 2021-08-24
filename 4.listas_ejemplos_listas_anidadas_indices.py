501dddca4a59df09af841c39da15d0af7e4e43da
Diego Beltran <68976175+diegoamb03@users.noreply.github.com>
2021-08-23 19:25:02 -0500

    Update 4.listas_ejemplos_listas_anidadas_indices.py
    ejemplos de lsita

--- 4.listas_ejemplos_listas_anidadas_indices.py
+++ 4.listas_ejemplos_listas_anidadas_indices.py
@@ -2,7 +2,7 @@ fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries
 
 fruits[-1] = "Melons"
 fruits.append("Lemons")
-print(fruits)
+#print(fruits)
 
 
 #ejemplos de listas anidadas.
@@ -11,7 +11,13 @@ fruits_2  = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherr
 vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
  
 dirty_dozen = [fruits, vegetables]
- 
+
+#como manejar los indices de las listas anidadas
+print(dirty_dozen)
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3]) 
 print(dirty_dozen[1][1])
 
 #ejemplo de listas anidadas
