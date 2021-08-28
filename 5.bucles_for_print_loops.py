bc67d93f0c57265ac09480a9e568644a4bc5b954
Diego Beltran <68976175+diegoamb03@users.noreply.github.com>
2021-08-28 14:07:42 -0500

    Create 5.bucles_for_print_loops.py
    ejemplos practicos

---
+++ 5.bucles_for_print_loops.py
@@ -0,0 +1,15 @@
+#For Loop with Lists
+frutas = ["Manzana", "Pera", "Guayaba"]
+for fruta in frutas:
+  print("Pie de " + fruta )
+
+paises_por_conocer = ["Brasil", "Ecuador", "Chile", "Argentina", "Panama", "Republica Dominicana", "Mexico", "Peru", "Guatemala"]
+
+for pais in paises_por_conocer:
+        print("Tengo que visitar " + pais)
+
+
+lenguajes_por_aprender = ["Js", "Python", "RStudio", "ios", "Android", "Php"]
+
+for lenguaje in lenguajes_por_aprender:
+        print("Aprendere " + lenguaje + " El resto de mis dias")
