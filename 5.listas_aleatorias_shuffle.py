e0743d4c13eacd4c3e37f9b0f8670e384d0a251f
Diego Beltran <68976175+diegoamb03@users.noreply.github.com>
2021-08-30 18:26:27 -0500

    Create 5.listas_aleatorias_shuffle.py
    Desordenar lista

---
+++ 5.listas_aleatorias_shuffle.py
@@ -0,0 +1,18 @@
+import random
+
+#ejemplos de .shuffle
+
+factura = ['pan', 'huevos', 100, 1234]
+lista = [1, 2.5, 'DevCode', [5,6] ,4]
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+simbolos = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+
+s=list(range(5))
+random.shuffle(s) # << shuffle before print or assignment
+print(s)
+
+random.shuffle(numeros)
+print(numeros)
