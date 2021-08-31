4e320fae554a54e19356c2923732f529c4118743
Diego Beltran <68976175+diegoamb03@users.noreply.github.com>
2021-08-30 19:32:18 -0500

    Create 6.robot_funciones.py
    Ejercicios con el Robot

---
+++ 6.robot_funciones.py
@@ -0,0 +1,19 @@
+"""https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json"""
+
+
+
+#ejercicios con el robot
+def ir_izquierda():
+    turn_left()
+    turn_left()
+    turn_left()
+
+   
+turn_left()
+move()
+ir_izquierda()
+move()
+ir_izquierda()
+move()
+ir_izquierda()
+move()
