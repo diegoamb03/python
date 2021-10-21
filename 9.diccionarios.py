ef30a0216d9586f70243265055736979dbf93969
Diego Beltran <68976175+diegoamb03@users.noreply.github.com>
2021-10-21 07:02:33 -0500

    Create 9.diccionarios.py

---
+++ 9.diccionarios.py
@@ -0,0 +1,54 @@
+#definicion de diccionarios
+
+
+diccionario_ejemplo = {
+  "key1" : "valor3",
+  "key2" : "valor2",
+  "key3" : "valor3"
+}
+print(diccionario_ejemplo)
+
+#imprimir una clave del diccionario
+print(diccionario_ejemplo["key2"])
+
+diccionario_ejemplo2 = {
+  "clave1":"valor1",
+  "clave2":"valor2",
+  "clave3":"valor3"
+}
+#imprimir una clave del diccionario
+print(diccionario_ejemplo2["clave3"])
+
+diccionario_ejemplo3 = {
+  "paises a visitar" : "Australia, nueva zelanda, Mexico, Panama",
+  "lenguajes de programacion a aprender" : "Pyton, JS, m, R" 
+}
+
+"""añadiendo nuevos items al diccionario"""
+diccionario_vacio ={
+  
+}
+
+#agregar claves al diccionario
+
+diccionario_vacio["clave_agregada"] = "valor1"
+diccionario_vacio["clave_agregada2"] = "valor2"
+diccionario_vacio["clave_agregada3"] = "valor3"
+
+
+#editar una clave agregada o añadir una clave
+
+diccionario_vacio["clave_agregada3"] = "valor modificado"
+
+print(diccionario_vacio)
+
+#ciclos trabajados con diccionarios
+
+"""imprimira las claves solamente"""
+
+for thing in diccionario_ejemplo3:
+  print(thing)
+
+"""imprimira los valores de las claves"""
+for key in diccionario_ejemplo3:
+  print(diccionario_ejemplo3[key])
